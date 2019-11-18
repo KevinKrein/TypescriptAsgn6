1ece6f0bac0ad6a63dfc0fb1a4f4716f2bc5aefc
alexarriola <aarriola@calpoly.edu>
2019-11-18 15:37:27 -0800

    tests

--- src/test.ts
+++ src/test.ts
@@ -1,12 +1,13 @@
-import interp from './asgn6';
-import topEnv from './asgn6';
+import { interp } from './asgn6';
 import { expect } from 'chai';
 // if you used the '@types/mocha' method to install mocha type definitions, uncomment the following line
 // import 'mocha';
 
 describe('Hello function', () => {
-  it('should return hello world', () => {
-    const result = interp(new NumC(5), topEnv);
-    expect(result).to.equal('Hello World!');
-  });
+  it('should return a number', () => {
+    const result = topInterp(new AppC(new LamC(["x", "y"], new AppC(new IdC("+"), 
+    [new IdC("x"), new IdC("y") ))) (list (NumC 6) (NumC 4))));
+
+    expect(result).to.equal("3");
+  })
 });
